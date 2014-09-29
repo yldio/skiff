3e115be6213b79ac730886cfdc20159fe77f9735
Pedro Teixeira <i@pgte.me>
2014-09-29 11:13:19 +0100

    node base api

---
+++ tests/node.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var Lab = require('lab');
+var lab = exports.lab = Lab.script();
+var describe = lab.describe;
+var it = lab.it;
+var assert = Lab.assert;
+var Node = require('../');
+
+describe('node', function() {
+
+  it('can create node with new and without options', function(done) {
+    var node = Node();
+    assert.instanceOf(node, Node);
+    done();
+  });
+
+	it('can create node without new and with no options', function(done) {
+    var node = Node();
+    assert.instanceOf(node, Node);
+    assert.typeOf(node.options, 'object');
+    done();
+	});
+});
