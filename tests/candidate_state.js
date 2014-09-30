33cb3c75da0fd15fd082e8c74d4c4ea0314b2c63
Pedro Teixeira <i@pgte.me>
2014-09-30 15:03:49 +0100

    testing if reaches leader state if can get all to vote yes

--- tests/candidate_state.js
+++ tests/candidate_state.js
@@ -8,6 +8,7 @@ var assert = Lab.assert;
 
 var uuid = require('cuid');
 var Node = require('./_node');
+var transport = require('./_transport');
 
 describe('candidate state', function() {
 
@@ -25,13 +26,15 @@ describe('candidate state', function() {
     });
   });
 
-  it('reaches doesnt reach leader if cannot get majority', function(done) {
+  it('doesnt reach leader if cannot get majority', function(done) {
     var node = Node();
     var remotes = [uuid(), uuid()];
 
     remotes.forEach(function(id) {
       node.join(id);
     });
+
+    node.on('error', console.log);
 
     var states = ['follower', 'candidate', 'candidate', 'candidate'];
 
@@ -42,6 +45,35 @@ describe('candidate state', function() {
         done();
       }
     });
+  });
+
+  it('reaches leader if can get all the votes', function(done) {
+    var node = Node();
+    var remotes = [uuid(), uuid()];
+    remotes.forEach(function(id) {
+      transport.listen(id, listen);
+      node.join(id);
+    });
+
+    var states = ['follower', 'candidate', 'leader'];
+
+    node.on('state', function(state) {
+      assert.equal(state, states.shift());
+      if (! states.length) {
+        assert.equal(node.commonState.persisted.currentTerm, 1);
+        done();
+      }
+    });
+
+    var voteRequests = 0;
+
+    function listen(type, args, cb) {
+      assert.equal(type, 'RequestVote');
+      assert.equal(args.term, 1);
+      assert.equal(args.lastLogIndex, 1);
+      assert((++ voteRequests) <= remotes.length);
+      cb(null, {voteGranted: true});
+    }
   });
 
 });
