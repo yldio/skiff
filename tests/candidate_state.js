64f599e7a7cdd35967b54c5f68852757feef785d
Pedro Teixeira <i@pgte.me>
2014-09-30 14:23:48 +0100

    asserting that leadership is not reached without majority

--- tests/candidate_state.js
+++ tests/candidate_state.js
@@ -6,7 +6,9 @@ var describe = lab.describe;
 var it = lab.it;
 var assert = Lab.assert;
 
+var uuid = require('cuid');
 var Node = require('./_node');
+var transport = require('./_transport');
 
 describe('candidate state', function() {
 
@@ -23,4 +25,24 @@ describe('candidate state', function() {
       }
     });
   });
+
+  it('reaches doesnt reach leader if cannot get majority', function(done) {
+    var node = Node();
+    var remotes = [uuid(), uuid()];
+
+    remotes.forEach(function(id) {
+      node.join(id);
+    });
+
+    var states = ['follower', 'candidate', 'candidate', 'candidate'];
+
+    node.on('state', function(state) {
+      assert.equal(state, states.shift());
+      if (! states.length) {
+        assert.equal(node.commonState.persisted.currentTerm, 2);
+        done();
+      }
+    });
+  });
+
 });
