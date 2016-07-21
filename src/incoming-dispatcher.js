cd0b84b93de872bd1437921bff9854f834ebdd07
Pedro Teixeira <i@pgte.me>
2016-07-21 18:11:00 +0100

    incoming dispatcher

---
+++ src/incoming-dispatcher.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const Writable = require('stream').Writable
+const merge = require('deepmerge')
+
+const defaultOptions = {
+  maxPending: 100,
+  objectMode: true
+}
+
+class Dispatcher extends Writable {
+
+  constructor (_options) {
+    const options = merge(defaultOptions, _options)
+    super(options)
+    this._options = options
+    this._pending = []
+  }
+
+  next (message) {
+    return this._pending.shift()
+  }
+
+  _write (message, _, callback) {
+    this._pending.push(message)
+    this._cap()
+    callback()
+    this.emit('readable')
+  }
+
+  _cap () {
+    // cap at the bottom, remove the oldest messages if we need space
+    if (this._pending.length > this._options.maxPending) {
+      this._pending.splice(0, this._pending.length - this._options.maxPending)
+    }
+  }
+
+}
+
+module.exports = Dispatcher
