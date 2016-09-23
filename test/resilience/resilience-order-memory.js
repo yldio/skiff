0cf0b741cb3a76488b07d104d21b82a1f8de6d4d
Pedro Teixeira <i@pgte.me>
2016-09-23 20:33:54 +0100

    higher setup and teardown timeouts because virtualbox

--- test/resilience/resilience-order-memory.js
+++ test/resilience/resilience-order-memory.js
@@ -14,8 +14,8 @@ const Client = require('../setup/client')
 
 describe('resilience, no chaos, in memory', () => {
   const setup = Setup({chaos: false})
-  before({timeout: 20000}, setup.before)
-  after({timeout: 20000}, setup.after)
+  before({timeout: 30000}, setup.before)
+  after({timeout: 30000}, setup.after)
 
   it ('works', {timeout: 121000}, done => {
     let timeout
